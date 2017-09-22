a7cca69ae26c1e27831f8d56072049b144e30256
pclundaahl <pc.lundaahl@gmail.com>
2017-09-21 18:05:24 -0700

    Refactored var names in tileRipple

--- script.js
+++ script.js
@@ -1,42 +1,86 @@
-let stdout = document.getElementById("output").innerHTML;
+const STDOUT      = document.getElementById("output").innerHTML;
+const CONFIG_SCALE = 1000;
 let print   = function(str) {document.getElementById("output").innerHTML += str; };
 let println = function(str) {print(str + "<br>"); };
 let clear = function(str) {document.getElementById("output").innerHTML = ""};
 
-// Ripple config
-let decay = 0.08;
-let dispersion = 2;
-let sink = 0.5;
+let decay;
+let dispersion;
+let sink;
+let canvas;
+let context;
 
-// Set up canvas
-let canvas = document.getElementById("main");
-let context = canvas.getContext("2d");
-let mouseEvent;
+// INITIALIZE. Called before the main app loop begins.
+function init () {
+  attachListeners();
+  initGui();
+  configurePhysics();
+  tileBuildListener();
 
-let testShade = 15;
-
-
-let tiles = [];
-
-for (i = 0; i < 50; i++) {
-  tiles[i] = [];
-  for (j = 0; j < 50; j++) {
-    tiles[i][j] = 0;
-  }
+  // Set up canvas
+  canvas = document.getElementById("main");
+  context = canvas.getContext("2d");
 }
 
+// MAIN LOOP. Performs repeated drawing and ripple calculations.
 function mainLoop () {
   setTimeout(function (){
 
-    ripple();
+    tileDecay();
     draw();
 
     mainLoop();
   }, 50);
 }
 
+// ATTACH EVENT LISTENERS
+function attachListeners() {
+  document.getElementById("main").onmousemove          = mouseMoveListener;
+  document.getElementById("configTiles").oninput       = tileBuildListener;
+  document.getElementById("configDecay").oninput       = configurePhysics;
+  document.getElementById("configDispersion").oninput  = configurePhysics;
+  document.getElementById("configSink").oninput        = configurePhysics;
+}
 
-function ripple() {
+// Updates tiles in response to mouse movement.
+function mouseMoveListener(event){
+  let tileW = canvas.width  / tiles.length;
+  let tileH = canvas.height / tiles[0].length;
+  tiles[Math.floor(event.x / tileW)][Math.floor(event.y / tileH)] = 1;
+}
+
+function tileBuildListener(){
+  let numberOfTiles = document.getElementById("configTiles").value;
+  buildTiles(numberOfTiles,numberOfTiles);
+}
+
+function initGui() {
+  document.getElementById("configDecay").value      += 0;
+  document.getElementById("configDispersion").value += 0;
+  document.getElementById("configSink").value       += 0;
+}
+
+// Configures variables that scale physics calculations.
+function configurePhysics() {
+  decay      = document.getElementById("configDecay").value      / CONFIG_SCALE;
+  dispersion = document.getElementById("configDispersion").value / CONFIG_SCALE;
+  sink       = document.getElementById("configSink").value       / CONFIG_SCALE;
+}
+
+// Assembles 2d array of tiles.
+function buildTiles(width, height) {
+  tiles = [];
+
+  for (i = 0; i < width; i++) {
+    tiles[i] = [];
+    for (j = 0; j < height; j++) {
+      tiles[i][j] = 0;
+    }
+  }
+}
+
+// Iterates through tiles and applies decay and ripple effects.
+function tileDecay() {
   let tileW = canvas.width / tiles.length;
   let tileH = canvas.height / tiles[0].length;
 
@@ -47,29 +91,36 @@ function ripple() {
         let decayAmt = tiles[i][j] * decay;
         tiles[i][j] -= decayAmt;
 
-        // Ripple to neighbors
-        for (k = i-1; k <= i+1; k++){
-          if (k >= 0 && k < tiles.length) {
-            for (l = j-1; l <= j+1; l++){
-              if (l >= 0 && l < tiles[0].length && !(k == i && l == j)) {
-
-                let difference = tiles[k][l] - tiles[i][j];
-
-                if (difference < 0) {
-                  tiles[k][l] += decayAmt * (difference * -1 ) * dispersion;
-                  tiles[k][l] %= 1;
-                }
-
-              }
-            }
-          }
-        }
-
+        tileRipple(i, j, decayAmt);
       }
     }
   }
 }
 
+// Ripple to neighbors
+function tileRipple(srcX, srcY, decayAmt) {
+  for (k = srcX-1; k <= srcX+1; k++){
+      for (l = srcY-1; l <= srcY+1; l++){
+
+        if (l * k < 0
+         || k >= tiles.length
+         || l >= tiles[0].length
+         || (k == srcX && l == srcY)) {
+        } else {
+          let difference = tiles[k][l] - tiles[srcX][srcY];
+
+          if (difference < 0) {
+            tiles[k][l] += decayAmt * (difference * -1 ) * dispersion;
+            tiles[k][l] %= 1;
+          }
+
+        }
+      }
+    // }
+  }
+}
+
+// Redraws the canvas
 function draw() {
   let tileW = canvas.width / tiles.length;
   let tileH = canvas.height / tiles[0].length;
@@ -78,7 +129,7 @@ function draw() {
   for (i = 0; i < tiles.length; i++) {
     for (j = 0; j < tiles[i].length; j++) {
       context.beginPath();
-      context.fillStyle = getShade(tiles[i][j]);
+      context.fillStyle = getColor(tiles[i][j]);
       context.rect(
         (tileW * i) + (tileW * tiles[i][j] * sink / 2)
        ,(tileH * j) + (tileH * tiles[i][j] * sink / 2)
@@ -92,8 +143,8 @@ function draw() {
   }
 }
 
-function getShade(value) {
-  // exp = 1 - Math.sqrt(value);
+// Returns a color code in hex string format.
+function getColor(value) {
   exp = 1;
   value *= 10;
   let coarse = Math.floor((value % 16) * exp);
@@ -107,16 +158,6 @@ function getShade(value) {
          + "" + (fine).toString(16));
 }
 
-document.getElementById("main").onmousemove = function(event){
-  let tileW = canvas.width  / tiles.length;
-  let tileH = canvas.height / tiles[0].length;
 
-  tiles[Math.floor(event.x / tileW)][Math.floor(event.y / tileH)] = 1;
-
-  clear();
-  print();
-};
-
+init();
 mainLoop();
-
-print("test");
